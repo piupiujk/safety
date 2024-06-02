55cced3c8575453bf102a01b575d9d09e3818043
piupiujk <docs4p@yandex.ru>
2024-06-02 18:53:30 +0500

    added user registration and authorization

--- main.py
+++ main.py
@@ -1,33 +1,40 @@
-from typing import List
+from fastapi import FastAPI, Depends
+from fastapi_users import FastAPIUsers
 
-from fastapi import FastAPI
-from pydantic import BaseModel
+from auth.auth import auth_backend
+from auth.database import User
+from auth.manager import get_user_manager
+from auth.schemas import UserRead, UserCreate
+
+fastapi_users = FastAPIUsers[User, int](
+    get_user_manager,
+    [auth_backend],
+)
 
 app = FastAPI(
     title='Safety'
 )
 
-fake_users = [
-    {'id': 1, 'role': 'standard', 'name': 'Дмитрий', 'connection': '88005553535'},
-    {'id': 2, 'role': 'admin', 'name': 'Александр', 'connection': '89068350343'},
-    {'id': 3, 'role': 'standard', 'name': 'Василий', 'connection': '89063843035'},
-]
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/auth/jwt",
+    tags=["auth"],
+)
+
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
+
+current_user = fastapi_users.current_user()
 
 
-class User(BaseModel):
-    id: int
-    role: str
-    name: str
-    connection: str
+@app.get("/protected-route")
+def protected_route(user: User = Depends(current_user)):
+    return f"Hello, {user.username}"
 
 
-@app.get('/users/{user_id}', response_model=List[User])
-def get_user(user_id: int):
-    return [user for user in fake_users if user.get('id') == user_id]
-
-
-@app.post('/users/{user_id}')
-def change_user_name(user_id: int, new_name: str):
-    current_user = list(filter(lambda user: user.get('id') == user_id, fake_users))[0]
-    current_user['name'] = new_name
-    return {'status': 200, 'data': current_user}
+@app.get("/unprotected-route")
+def unprotected_route():
+    return f"Hello, anonym"
